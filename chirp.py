5e6e3e0511d719f47c5d28fa3e2df65c672fdb86
rockyrockz95 <jscott003@citymail.cuny.edu>
2023-10-29 23:18:35 -0400

    Added login and register forms

--- chirp.py
+++ chirp.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, url_for, redirect
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+
+# standard practice in Flask for security
+app.config['SECRET_KEY'] = 'c5d8384942a409d54c42eca4512864f7'
 
 
 @app.route("/")
@@ -12,15 +16,31 @@ def home():
 def account():
     return render_template("account.html")
 
-@app.route("/login")
+# passing in GET and POST methods allows us to submit data via the page
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "<h1>Login Info Here</h1>"
+    form = LoginForm() # instance of
+    if form.validate_on_submit():
+        flash("Account Created!")
+        return redirect(url_for("home"))
+    else:
+        flash("Login Unsuccessful. Check username and password")
+    return render_template("login.html", title="Login", form=form)
 
 
 @app.route("/register")
 def register():
-    return "<h1>Sign Up Here<h1>"
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash("Account Created!")
+        return redirect(url_for("login"))
+    else:
+        flash("Invalid email, username, or password. Try again")
+    return render_template("register.html", title="Register", form=form)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# TODO: Get the flashed message to show up
+# TODO: Check validators
---
+++ forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
+
+# creating each form as a class, possible using wtforms package
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    # No remember me functionality yet
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", 
+                        validators=[DataRequired(), Email()])
+    username = StringField("Username", 
+                           validators=[DataRequired(), Length(min=4, max=15)])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",
+                                     validators=[DataRequired(), EqualTo("password")])
+    
+
+# Sources: 
+#   validators: https://wtforms.readthedocs.io/en/2.3.x/validators/#:~:text=Validates%20that%20input%20was%20provided%20for%20this%20field.,required%20flag%20on%20fields%20it%20is%20used%20on.
