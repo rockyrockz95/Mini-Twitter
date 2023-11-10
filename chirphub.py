993afa52d7ec2380e500612fcda17a8f5280ca47
rockyrockz95 <jscott003@citymail.cuny.edu>
2023-11-10 16:48:01 -0500

    login route fix

--- chirphub.py
+++ chirphub.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, flash, url_for, redirect
-from flask_login import LoginManager, current_user, login_user, logout_user
-from forms import RegistrationForm, LoginForm
+import os
+from flask import Flask, render_template, flash, url_for, redirect, request
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from forms import RegistrationForm, LoginForm, EditAccountForm
 from data import User
 
 app = Flask(__name__)
@@ -19,6 +26,7 @@ login_manager.login_view = "login"
 # returns the integer described by the user_id, where user_id = email
 def user_loader(user_id):
     # integer indexed slicing of user dataFrame with email == user_id
+    # first row that fits the search
     user_data = User.users[User.users["email"] == user_id].iloc[0]
 
     user = User(
@@ -61,23 +69,30 @@ def login():
         return redirect(url_for("home"))
 
     form = LoginForm()  # instance of
-    print("Form data - Username:", form.username.data)
-    print("Form data - Password:", form.password.data)  # authetification issue test
     if form.validate_on_submit():
         # return row of matching username
-        user = User.users[User.users["username"] == form.username.data].iloc[0]
-        if form.password.data == user["password"]:
-            user_instance = User(
-                email=user["email"],
-                username=user["username"],
-                password=user["password"],
-                image_file=user["image_file"],
-                likes=user["likes"],
-                posts=user["posts"],
-            )
-            login_user(user_instance)
-            flash("Login Successful!", "info")
-            return redirect(url_for("home"))
+        # returns an error if incorrect username entered. oops
+        user = User.users[User.users["username"] == form.username.data]
+        if not user.empty:
+            user = user.iloc[0]
+            if form.password.data == user["password"]:
+                user_instance = User(
+                    email=user["email"],
+                    username=user["username"],
+                    password=user["password"],
+                    image_file=user["image_file"],
+                    likes=user["likes"],
+                    posts=user["posts"],
+                )
+                login_user(user_instance)
+                flash("Login Successful!", "info")
+                return redirect(url_for("home"))
+            else:
+                flash("Invalid username or password", "warning")
+                return redirect(url_for("login"))
+        else:
+            flash("Invalid username or password", "warning")
+            return redirect(url_for("login"))
 
     return render_template("login.html", title="Login", form=form)
 
@@ -95,12 +110,42 @@ def reset_request():
     return render_template("reset_request.html")
 
 
-@app.route("/account")
+# add pitcure file to sys
+# needed for editing the user profile picture
+def picture_path(image_file):
+    pass
+
+
+@app.route("/account", methods=["GET", "POST"])
+@login_required
 def account():
-    return render_template("account.html")
+    print(current_user.password)
+    """# checking if current_user credentials are acccurate to the logged in user (2x)
+          # user: admin --> password, username, id, image_file works,  works"""
+    form = EditAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            current_user.image_file = form.picture.data
+            """ Plan: Edit account features
+                   Grab the current user row :
+                      user = User.users[User.users["username"] == form.username.data].iloc[0]
+                      change the parameters entered
+                      put the user in the database with the new parameters
+                      delete the original"""
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        flash("Your account has been updated", "info")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-# TODO: Check validators
+# TODO: Add sources for image file and os functions
